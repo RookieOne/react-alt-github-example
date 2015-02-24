a7510b8b914e4fea17593c2a2d95247ac27b4dfd
Jonathan Birkholz <rookieone@gmail.com>
2015-02-24 14:04:03 -0600

    remove node_modules from repo

--- node_modules/react-router/lib/Cancellation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-/**
- * Represents a cancellation caused by navigating away
- * before the previous transition has fully resolved.
- */
-function Cancellation() {}
-
-module.exports = Cancellation;
